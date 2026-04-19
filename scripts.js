fa93f93bb3a8441fd0dbebe7f5a4b78440e6d153
dangtruong06 <DANGTT066@GMAIL.COM>
2026-04-18 19:27:57 -0700

    first commit of app and working operations

--- scripts.js
+++ scripts.js
@@ -23,21 +23,474 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+//MY DATA SET
+champions_data_set = [
+  {
+      name: 'Ahri',
+      cost: 3,
+      traits: ['Star Guardian', 'Sorcerer'],
+      type: 'Mage',
+      image: 'images/ahri.png'
+  },
+  {
+      name: 'Aatrox',
+      cost: 1,
+      traits: ['Mighty Mech', 'Juggernaut', 'Heavyweight'],
+      type: 'Frontline Fighter',
+      image: 'images/aatrox.png'
+  },
+  
+  {
+      name: 'Akali',
+      cost: 4,
+      traits: ['Supreme Cells', 'Executioner'],
+      type: 'Mage Assassin',
+      image: 'images/akali.png'
+  },
+  {
+      name: 'Ashe',
+      cost: 4,
+      traits: ['Crystal Gambit', 'Duelist'],
+      type: 'Backline Carry',
+      image: 'images/ashe.png'
+  },
+  {
+      name: 'Braum',
+      cost: 5,
+      traits: ['Luchador', 'Bastion'],
+      type: 'Frontline Fighter',
+      image: 'images/braum.png'
+  },
+  {
+      name: 'Caitlyn',
+      cost: 3,
+      traits: ['Battle Academia', 'Sniper'],
+      type: 'Backline Carry',
+      image: 'images/caitlyn.png'
+  },
+  {
+      name: 'Darius',
+      cost: 3,
+      traits: ['Supreme Cells', 'Heavyweight'],
+      type: 'Frontline Fighter',
+      image: 'images/darius.png'
+  },
+  {
+      name: 'Ezreal',
+      cost: 1,
+      traits: ['Battle Academia', 'Prodigy'],
+      type: 'Mage',
+      image: 'images/ezreal.png'
+  },
+  {
+      name: 'Gangplank',
+      cost: 2,
+      traits: ['Duelist', 'Mighty Mech'],
+      type: 'Frontline Fighter',
+      image: 'images/gangplank.png'
+  },
+  {
+      name: 'Garen',
+      cost: 1,
+      traits: ['Battle Academia', 'Bastion'],
+      type: 'Frontline Tank',
+      image: 'images/garen.png'
+  },
+  {
+      name: 'Gnar',
+      cost: 1,
+      traits: ['Luchador', 'Sniper'],
+      type: 'Backline Carry',
+      image: 'images/gnar.png'
+  },
+  {
+      name: 'Gwen',
+      cost: 5,
+      traits: ['Soul Fighter', 'Sorcerer'],
+      type: 'Mage Assassin',
+      image: 'images/gwen.png'
+  },
+  {
+      name: 'Janna',
+      cost: 2,
+      traits: ['Protector', 'Strategist', 'Crystal Gambit'],
+      type: 'Mage',
+      image: 'images/janna.png'
+  },
+  {
+      name: 'Jayce',
+      cost: 3,
+      traits: ['Heavyweight', 'Battle Academia'],
+      type: 'Frontline Fighter',
+      image: 'images/jayce.png'
+  },
+  {
+      name: 'Jarvan',
+      cost: 4,
+      traits: ['Mighty Mech', 'Strategist'],
+      type: 'Frontline Tank',
+      image: 'images/jarvan.png'
+  },
+  {
+      name: 'Jhin',
+      cost: 2,
+      traits: ['Wraith', 'Sniper'],
+      type: 'Backline Carry',
+      image: 'images/jhin.png'
+  },
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+  {
+      name: 'Jinx',
+      cost: 4,
+      traits: ['Star Guardian', 'Sniper'],
+      type: 'Backline Carry',
+      image: 'images/jinx.png'
+  },
+  {
+      name: 'Kai\'sa',
+      cost: 2,
+      traits: ['Duelist', 'Supreme Cells'],
+      type: 'Backline Carry',
+      image: 'images/kaisa.png'
+  },
+  {
+      name: 'Kalista',
+      cost: 1,
+      traits: ['Soul Fighter', 'Executioner'],
+      type: 'Backline Carry',
+      image: 'images/kalista.png'
+  },
+  {
+      name: 'Karma',
+      cost: 4,
+      traits: ['Mighty Mech', 'Sorcerer'],
+      type: 'Mage',
+      image: 'images/karma.png'
+  },
+  {
+      name: 'Katarina',
+      cost: 2,
+      traits: ['Battle Academia', 'Executioner'],
+      type: 'Mage Assassin',
+      image: 'images/katarina.png'
+  },
+  {
+      name: 'Kayle',
+      cost: 1,
+      traits: ['Wraith', 'Duelist'],
+      type: 'Backline Carry',
+      image: 'images/kayle.png'
+  },
+  {
+      name: 'Kennen',
+      cost: 1,
+      traits: ['Supreme Cells', 'Protector', 'Sorcerer'],
+      type: 'Frontline Tank',
+      image: 'images/kennen.png'
+  },
+  {
+      name: 'Kobuko',
+      cost: 2,
+      traits: ['Heavyweight', 'Mentor'],
+      type: 'Frontline Tank',
+      image: 'images/kobuko.png'
+  },
+  {
+      name: 'Kogmaw',
+      cost: 3,
+      traits: ['Monster Trainer'],
+      type: 'Backline Carry',
+      image: 'images/kogmaw.png'
+  },
+  {
+      name: 'K\'sante',
+      cost: 4,
+      traits: ['Wraith', 'Protector'],
+      type: 'Frontline Fighter',
+      image: 'images/ksante.png'
+  },
+  {
+      name: 'Lee Sin',
+      cost: 5,
+      traits: ['Stance Master', 'Duelist'],
+      type: 'Assassin',
+      image: 'images/leesin.png'
+  },
+  {
+      name: 'Leona',
+      cost: 4,
+      traits: ['Battle Academia', 'Bastion'],
+      type: 'Frontline Tank',
+      image: 'images/leona.png'
+  },
+  {
+      name: 'Lucian',
+      cost: 1,
+      traits: ['Sorcerer', 'Mighty Mech'],
+      type: 'Mage',
+      image: 'images/lucian.png'
+  },
+  {
+      name: 'Lux',
+      cost: 2,
+      traits: ['Soul Fighter', 'Sorcerer'],
+      type: 'Mage',
+      image: 'images/lux.png'
+  },
+  {
+      name: 'Malphite',
+      cost: 1,
+      traits: ['The Crew', 'Protector'],
+      type: 'Frontline Tank',
+      image: 'images/malphite.png'
+  },
+  {
+      name: 'Malzahar',
+      cost: 3,
+      traits: ['Wraith', 'Prodigy'],
+      type: 'Mage',
+      image: 'images/malzahar.png'
+  },
+  {
+      name: 'Dr.Mundo',
+      cost: 2,
+      traits: ['Luchador', 'Juggernaut'],
+      type: 'Frontline Tank',
+      image: 'images/mundo.png'
+  },
+  
+  {
+      name: 'Naafiri',
+      cost: 1,
+      traits: ['Soul Fighter', 'Juggernaut'],
+      type: 'Assassin',
+      image: 'images/naafiri.png'
+  },
+  {
+      name: 'Neeko',
+      cost: 3,
+      traits: ['Star Guardian', 'Protector'],
+      type: 'Frontline Tank',
+      image: 'images/neeko.png'
+  },
+  {
+      name: 'Poppy',
+      cost: 4,
+      traits: ['Star Guardian', 'Heavyweight'],
+      type: 'Frontline Tank',
+      image: 'images/poppy.png'
+  },
+  {
+      name: 'Rakan',
+      cost: 2,
+      traits: ['Protector', 'Battle Academia'],
+      type: 'Frontline Tank',
+      image: 'images/rakan.png'
+  },
+  {
+      name: 'Rammus',
+      cost: 3,
+      traits: ['Monster Trainer'],
+      type: 'Frontline Tank',
+      image: 'images/rammus.png'
+  },
+  {
+      name: 'Rell',
+      cost: 1,
+      traits: ['Star Guardian', 'Bastion'],
+      type: 'Frontline Tank',
+      image: 'images/rell.png'
+  },
+  {
+      name: 'Ryze',
+      cost: 4,
+      traits: ['Mentor', 'Strategist', 'Executioner'],
+      type: 'Mage',
+      image: 'images/ryze.png'
+  },
+  {
+      name: 'Samira',
+      cost: 4,
+      traits: ['Soul Fighter', 'Edgelord'],
+      type: 'Assassin',
+      image: 'images/samira.png'
+  },
+  {
+      name: 'Senna',
+      cost: 3,
+      traits: ['Mighty Mech', 'Executioner'],
+      type: 'Mage',
+      image: 'images/senna.png'
+  },
+  {
+      name: 'Seraphine',
+      cost: 5,
+      traits: ['Star Guardian', 'Prodigy'],
+      type: 'Mage',
+      image: 'images/seraphine.png'
+  },
+  {
+      name: 'Sett',
+      cost: 4,
+      traits: ['Soul Fighter', 'Juggernaut'],
+      type: 'Frontline Fighter',
+      image: 'images/sett.png'
+  },
+  {
+      name: 'Shen',
+      cost: 2,
+      traits: ['The Crew','Bastion', 'Edgelord'],
+      type: 'Frontline Tank',
+      image: 'images/shen.png'
+  },
+  {
+      name: 'Sivir',
+      cost: 1,
+      traits: ['The Crew', 'Sniper'],
+      type: 'Backline Carry',
+      image: 'images/sivir.png'
+  },
+  {
+      name: 'Smolder',
+      cost: 3,
+      traits: ['Monster Trainer'],
+      type: 'Mage',
+      image: 'images/smolder.png'
+  },
+  {
+      name: 'Swain',
+      cost: 3,
+      traits: ['Bastion','Crystal Gambit','Sorcerer'],
+      type: 'Frontline Tank',
+      image: 'images/swain.png'
+  },
+  {
+      name: 'Syndra',
+      cost: 1,
+      traits: ['Star Guardian', 'Prodigy','Crystal Gambit'],
+      type: 'Mage',
+      image: 'images/syndra.png'
+  },
+  {
+      name: 'Twisted Fate',
+      cost: 5,
+      traits: ['The Crew'],
+      type: 'Backline Carry',
+      image: 'images/twistedfate.png'
+  },
+  {
+      name: 'Vi',
+      cost: 2,
+      traits: ['Crystal Gambit', 'Juggernaut'],
+      type: 'Frontline Fighter',
+      image: 'images/vi.png'
+  },
+  {
+      name: 'Viego',
+      cost: 3,
+      traits: ['Soul Fighter', 'Duelist'],
+      type: 'Assassin',
+      image: 'images/viego.png'
+  },
+  {
+      name: 'Volibear',
+      cost: 4,
+      traits: ['Luchador', 'Edgelord'],
+      type: 'Frontline Fighter',
+      image: 'images/volibear.png'
+  },
+  {
+      name: 'Xayah',
+      cost: 2,
+      traits: ['Star Guardian', 'Edgelord'],
+      type: 'Backline Carry',
+      image: 'images/xayah.png'
+  },
+  {
+      name: 'Xin Zhao',
+      cost: 2,
+      traits: ['Soul Fighter', 'Bastion'],
+      type: 'Frontline Fighter',
+      image: 'images/xinzhao.png'
+  },
+  {
+      name: 'Yasuo',
+      cost: 3,
+      traits: ['Mentor', 'Edgelord'],
+      type: 'Assassin',
+      image: 'images/yasuo.png'
+  },
+  {
+      name: 'Yone',
+      cost: 5,
+      traits: ['Mighty Mech', 'Edgelord'],
+      type: 'Assassin',
+      image: 'images/yone.png'
+  },
+  {
+      name: 'Yuumi',
+      cost: 4,
+      traits: ['Battle Academia','Prodigy'],
+      type: 'Mage',
+      image: 'images/yuumi.png'
+  },
+  {
+      name: 'Zac',
+      cost: 1,
+      traits: ['Wraith', 'Heavyweight'],
+      type: 'Frontline Tank',
+      image: 'images/zac.png'
+  },
+  {
+      name: 'Ziggs',
+      cost: 4,
+      traits: ['The Crew', 'Strategist'],
+      type: 'Mage',
+      image: 'images/ziggs.png'
+  },
+  {
+      name: 'Zyra',
+      cost: 5,
+      traits: ['Crystal Gambit'],
+      type: 'Mage',
+      image: 'images/zyra.png'
+  },
+  {
+      name: 'Udyr',
+      cost: 3,
+      traits: ['Juggernaut', 'Mentor', 'Duelist'],
+      type: 'Frontline Fighter',
+      image: 'images/udyr.png'
+  },
+  {
+      name: 'Varus',
+      cost: 5,
+      traits: ['Wraith', 'Sniper'],
+      type: 'Backline Carry',
+      image: 'images/varus.png'
+  }
+
+]
+
+team = []
+
+//Copy of dataset for filtering
+let current_data_set = [...champions_data_set];
+
+
+//function to sort displayed cards by champion's buy cost
+function sortByCost(){
+  current_data_set = current_data_set.sort((a,b) => a.cost - b.cost)
+  showCards()
+  
+}
+
+//reset function to clear filters on cards
+function reset(){
+  current_data_set = [...champions_data_set]
+  showCards()
+  
+}
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -45,53 +498,221 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  current_data_set.forEach((champion) => {
+    const nextCard = templateCard.cloneNode(true)
+    editCardContent(nextCard, 
+                    champion.name, 
+                    champion.image, 
+                    champion.traits, 
+                    champion.cost, 
+                    champion.type)
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
-  }
+    cardContainer.appendChild(nextCard)
+  })
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+
+// This function creates the card's layout using data passed in
+function editCardContent(card, newTitle, newImageURL, traits, cost, type) {
   card.style.display = "block";
 
-  const cardHeader = card.querySelector("h2");
+  const cardHeader = card.querySelector("h1");
+  let color;
+  if(cost == 2){
+    color = 'green'
+  }
+  else if(cost == 3){
+    color = 'blue'
+  }
+  else if(cost == 4){
+    color = 'purple'
+  }
+  else if(cost == 5){
+    color = 'yellow'
+  }
+    
   cardHeader.textContent = newTitle;
+  cardHeader.style.color = color;
+  card.style.boxShadow = `0 20px 15px ${color}`;
 
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
+  const ul = card.querySelector("ul");
+  //Clear the ul element first
+  ul.innerHTML = `
+    <li>Cost: ${cost}</li>
+    <li>Type: ${type}</li>
+    <li>Traits: ${traits.join(", ")}</li>
+  `;
+
   console.log("new card:", newTitle, "- html: ", card);
 }
 
+/**
+ * this function will let users search for champions, then create those champion's cards
+ * as search results
+ * Cards are clickable to add to team builder
+ */
+function displaySearch(result){
+  const searchResult = document.getElementById('search-result')
+  searchResult.innerHTML = ""
+
+  const templateCard = document.querySelector(".card");
+
+  result.forEach((champion) => {
+    const nextCard = templateCard.cloneNode(true)
+    editCardContent(nextCard, 
+                    champion.name, 
+                    champion.image, 
+                    champion.traits, 
+                    champion.cost, 
+                    champion.type)
+
+    nextCard.addEventListener('click', () => addToTeam(champion))
+    searchResult.appendChild(nextCard)
+
+  })
+
+}
+
+//function to add champions to team builder
+function addToTeam(champion){
+  if (team.length >= 9){
+    alert('Team is full')
+    return
+  }
+
+  if (team.find(champ => champ.name == champion.name)){
+    alert(`Champion is already in the team!`)
+    return
+  }
+  
+  team.push(champion)
+  updateTeamDisplay()
+}
+
+// Function to remove cards from team builder
+function removeFromTeam(champion){
+  //make a new team without the champion
+  team = team.filter(c => c.name !== champion.name)
+  updateTeamDisplay()
+}
+
+// function to update team builder display every time we add and remove card
+// from builder
+function updateTeamDisplay(){
+  const teamSlot = document.getElementById('team-slot')
+  teamSlot.innerHTML = "";
+
+  const templateCard = document.querySelector(".card");
+
+  team.forEach((champion) => {
+    const nextCard = templateCard.cloneNode(true)
+    editCardContent(nextCard, 
+                    champion.name, 
+                    champion.image, 
+                    champion.traits, 
+                    champion.cost, 
+                    champion.type)
+
+    
+    nextCard.addEventListener('click', () => removeFromTeam(champion))
+    teamSlot.appendChild(nextCard);
+    updateTraits()
+  })
+}
+
+
+// Function to select selected champions' traits and add them to a counter
+function updateTraits(){
+  const traitDisplay = document.querySelector('#right-panel h1');
+  const traitCounter = {}
+
+  team.forEach((champ) => {
+    champ.traits.forEach((trait) => {
+      //check if trait exists in team, if yes add 1 to counter
+      //For each loop does this for each trait
+      if(traitCounter[trait]){
+        traitCounter[trait] = traitCounter[trait] + 1;
+      }
+      else{
+        //if trait doesn't exist, create the trait and start it at 1
+        traitCounter[trait] = 1;
+      }
+
+      //get key value out of trait counter object
+      const traitString = Object.entries(traitCounter)
+      .map(([trait,num]) => `${trait}: ${num}`) //.map runs once every pair to turn it into a string
+      .join(' | ') 
+      
+      
+      if(traitString == ''){
+        traitDisplay.textContent = 'Active Traits: '
+      }
+      else{
+        traitDisplay.textContent = `Active Traits: ${traitString}`
+      }
+    })
+  })
+
+
+}
+
 // This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+document.addEventListener("DOMContentLoaded", function(){
+  const filterToggle = document.getElementById("filter-toggle");
+  const panel = document.getElementById("filter-panel")
+  const apply = document.getElementById("apply-filters")
 
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
-}
+  filterToggle.addEventListener('click', () =>{
+    panel.classList.toggle('hidden')
+    apply.classList.toggle('hidden')
+  });
 
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
+  
+  // Checks for filters
+  apply.addEventListener('click', ()=> {  
+    const costValue = document.getElementById('cost-select').value
+    const traitValue = document.getElementById('trait-select').value
+    const typeValue = document.getElementById('type-select').value
+
+    current_data_set = [...champions_data_set]
+
+    if(costValue !== ''){
+      current_data_set = current_data_set.filter(champ => champ.cost == costValue);
+    }
+
+    //for trait, we need to use includes since champions have multiple traits and is stored 
+    //in an array
+    if(traitValue !== ''){
+      current_data_set = current_data_set.filter(champ => champ.traits.includes(traitValue));
+    }
+
+
+    if(typeValue !== ''){
+      current_data_set = current_data_set.filter(champ => champ.type == typeValue);
+    }
+
+
+    showCards()
+  })  
+
+  const searchBar = document.getElementById('search-bar')
+  searchBar.addEventListener('input', ()=>{
+    
+    const search = searchBar.value.toLowerCase()
+
+    if (search === ''){
+      document.getElementById('search-result').innerHTML = ""
+      return
+    }
+    const result = champions_data_set.filter(champ => champ.name.toLowerCase().includes(search))
+
+    displaySearch(result)
+  })
+
+  showCards()
+
+});
